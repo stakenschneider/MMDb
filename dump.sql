00ef18171b5c05ffcc9df606fbf2a1b585d7f226
Volkova Maria <volkova.maria.d@gmail.com>
2017-12-05 21:43:44 +0300

    issue is closed
    ИЗМЕНЕНИЕ СХЕМЫ БД
    1. Ввести единообразный учет людей, участвующих в производстве кино.
    2. Добавить возможность участия человека в фильме в заданном качестве: актер, оператор, режиссер и пр.
    3. Добавить учет видов типов кинопремий (разные фестивали и номинации) и получения фильмами премий.

--- dump.sql
+++ dump.sql
@@ -1,129 +1,150 @@
 create table genres
 (
-	genreid serial not null
-		constraint genres_pkey
-			primary key,
-	genrename varchar(40) not null
+genreid serial not null
+constraint genres_pkey
+primary key,
+genrename varchar(40) not null
 )
 ;
 
 create unique index genres_genreid_uindex
-	on genres (genreid)
+on genres (genreid)
 ;
 
 create unique index genres_genrename_uindex
-	on genres (genrename)
+on genres (genrename)
 ;
 
 create table movie
 (
-	movieid serial not null
-		constraint movie_pkey
-			primary key,
-	name varchar(100) not null,
-	releaseyear integer,
-	rating numeric,
-	movielength integer,
-	description varchar(300)
+movieid serial not null
+constraint movie_pkey
+primary key,
+name varchar(100) not null,
+releaseyear integer,
+rating numeric,
+movielength integer,
+description varchar(300)
 )
-;
-
-create table actors
-(
-	actorid serial not null
-		constraint actors_pkey
-			primary key,
-	firstname varchar(30) not null,
-	lastname varchar(50),
-	birth date
-)
-;
-
-create unique index actors_actorid_uindex
-	on actors (actorid)
-;
-
-create table movieactors
-(
-	movieactorsid serial not null
-		constraint movieactors_pkey
-			primary key,
-	movieid serial not null
-		constraint movieactors_movie_movieid_fk
-			references movie,
-	actorid serial not null
-		constraint movieactors_actors_actorid_fk
-			references actors
-)
-;
-
-create unique index movieactors_movieactors_uindex
-	on movieactors (movieactorsid)
-;
-
-create table moviedirectors
-(
-	moviedirectorsid serial not null
-		constraint moviedirectors_pkey
-			primary key,
-	movieid serial not null
-		constraint moviedirectors_movie_movieid_fk
-			references movie,
-	directorid serial not null
-)
-;
-
-create unique index moviedirectors_moviedirectorsid_uindex
-	on moviedirectors (moviedirectorsid)
-;
-
-create table directors
-(
-	directorsid serial not null
-		constraint directors_pkey
-			primary key,
-	firstname varchar(30) not null,
-	lastname varchar(50),
-	birth date
-)
-;
-
-create unique index directors_directorsid_uindex
-	on directors (directorsid)
-;
-
-alter table moviedirectors
-	add constraint moviedirectors_directors_directorsid_fk
-		foreign key (directorid) references directors
 ;
 
 create table moviegenres
 (
-	moviegenres serial not null
-		constraint moviegenres_pkey
-			primary key,
-	movieid serial not null
-		constraint moviegenres_movie_movieid_fk
-			references movie,
-	genreid serial not null
-		constraint moviegenres_genres_genreid_fk
-			references genres
+moviegenres serial not null
+constraint moviegenres_pkey
+primary key,
+movieid serial not null
+constraint moviegenres_movie_movieid_fk
+references movie,
+genreid serial not null
+constraint moviegenres_genres_genreid_fk
+references genres
 )
 ;
 
 create unique index moviegenres_moviegenres_uindex
-	on moviegenres (moviegenres)
+on moviegenres (moviegenres)
 ;
 
-create table imagines
+create table people
 (
-	imaginesid serial not null
-		constraint imagines_pkey
-			primary key,
-	imgine bytea not null
+peopleid serial not null
+constraint people_pkey
+primary key,
+firstname varchar(50) not null,
+lastname varchar(50) not null,
+birth date
 )
 ;
 
-create unique index imagines_imaginesid_uindex
-	on imagines (imaginesid)
+create unique index people_peopleid_uindex
+on people (peopleid)
+;
+
+create table awards
+(
+awardsid serial not null
+constraint awards_pkey
+primary key,
+name varchar(100) not null
+)
+;
+
+create unique index awards_awardsid_uindex
+on awards (awardsid)
+;
+
+create unique index awards_name_uindex
+on awards (name)
+;
+
+create table moviepeople
+(
+moviepeopleid serial not null
+constraint moviepeople_pkey
+primary key,
+movieid serial not null
+constraint moviepeople_movie_movieid_fk
+references movie,
+peopleid serial not null
+constraint moviepeople_people_peopleid_fk
+references people
+)
+;
+
+create unique index moviepeople_moviepeopleid_uindex
+on moviepeople (moviepeopleid)
+;
+
+create table movieawards
+(
+movieawardsid serial not null
+constraint movieawards_pkey
+primary key,
+movieid serial not null
+constraint movieawards_movie_movieid_fk
+references movie,
+awardid serial not null
+constraint movieawards_awards_awardsid_fk
+references awards
+)
+;
+
+create unique index movieawards_movieawardsid_uindex
+on movieawards (movieawardsid)
+;
+
+create table profession
+(
+professionid serial not null
+constraint profession_pkey
+primary key,
+name varchar(70) not null
+)
+;
+
+create unique index profession_professionid_uindex
+on profession (professionid)
+;
+
+create unique index profession_name_uindex
+on profession (name)
+;
+
+create table peopleprofession
+(
+peopleprofessionid serial not null
+constraint peopleprofession_pkey
+primary key,
+peopleid serial not null
+constraint peopleprofession_people_peopleid_fk
+references people,
+professionid serial not null
+constraint peopleprofession_profession_professionid_fk
+references profession
+)
+;
+
+create unique index peopleprofession_peopleprofessionid_uindex
+on peopleprofession (peopleprofessionid)
 ;
