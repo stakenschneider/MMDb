26ab828afdc407ab0df3a6b85c2d392a77db81a6
Masha <volkova.maria.d@gmail.com>
2017-12-17 23:26:13 +0300

    closed #2

--- issue_#2.sql
+++ issue_#2.sql
@@ -1,12 +1,21 @@
 -- Вывести 10 человек, которые получали какие-либо премии за фильмы, в которых участвовали в двух или более качествах
-SELECT p.firstname , p.lastname ,  count(pp.peopleid) as count
-FROM moviepeople pp , people p
-WHERE p.peopleid = pp.peopleid
-GROUP BY p.peopleid
-ORDER BY count DESC
+SELECT firstname, lastname, count(moviepeople.professionid) as count_prof
+FROM moviepeople
+  JOIN people on (people.peopleid = moviepeople.peopleid)
+  WHERE awardid is not NULL
+GROUP BY moviepeople.movieid , moviepeople.peopleid , firstname,lastname
+ORDER BY count_prof DESC
 LIMIT 10;
 
 
 -- Вывести 10 самых успешных деятелей кино (максимальное количество наград) за заданный период времени.
 -- При одинаковом количестве наград вторым показателем успешности необходимо считать общее количество фильмов,
 -- в которых принималось участие.
+SELECT people.peopleid, people.firstname , people.lastname, count (moviepeople.awardid) as award_count , count(moviepeople.movieid) as movie_count
+FROM people
+  JOIN moviepeople on (people.peopleid = moviepeople.peopleid)
+  JOIN movie on (movie.movieid = moviepeople.movieid)
+WHERE releaseyear BETWEEN 1917 and 2016
+GROUP BY people.peopleid
+  ORDER BY award_count DESC , movie_count DESC
+LIMIT 10;
